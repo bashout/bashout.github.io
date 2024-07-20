e54cb38d5a593f20b3d5e8ec167929887a146670
Brian Toye <dev@bashout.work>
2024-07-20 20:02:07 +0100

    deployment for github pages configured

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://portfolio.bashout.work",
     integrations: [
         tailwind({
             // Example: Provide a custom path to a Tailwind config file
