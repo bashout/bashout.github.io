3ebb4dd195890de54ae863e7a4c8d11c3b07ba2b
Brian Toye <dev@bashout.work>
2024-07-20 19:41:25 +0100

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        tailwind({
+            // Example: Provide a custom path to a Tailwind config file
+            configFile: "./tailwind.config.js",
+        }),
+    ],
+});
