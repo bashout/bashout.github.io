6e04f72c03723aa0f29377d3e41d930536b3e4c2
Brian Toye <dev@bashout.work>
2024-08-11 23:19:52 +0100

    Bump vers and add icons package
    Add astro icons configuration and bump versions including an override required for astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,6 +9,13 @@ export default defineConfig({
         tailwind({
             // Example: Provide a custom path to a Tailwind config file
             configFile: "./tailwind.config.js",
+        }),
+        icon({
+            include: {
+                // Include only three `mdi` icons in the bundle
+                devicon: ["*"],
+                mdi: ["*"],
+            },
         }),
     ],
 });
