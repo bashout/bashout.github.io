67f6bd1039148fb6a06056b531c6a46e739776cd
Brian Toye <dev@bashout.work>
2024-08-13 17:13:48 +0100

    Add i18n support

---
+++ src/i18n/ui.ts
@@ -0,0 +1,17 @@
+export const languages = {
+    en: "English",
+    ru: "Русский",
+};
+
+export const defaultLang = "en";
+export const showDefaultLang = false;
+export const ui = {
+    en: {
+        title: "bashout.",
+        "nav.contact": "contact",
+    },
+    ru: {
+        title: "башаут.",
+        "nav.contact": "",
+    },
+} as const;
