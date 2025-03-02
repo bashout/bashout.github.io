c8bded407fea3b6266316f852b88b3608766e4ca
Brian Toye <dev@bashout.work>
2025-03-02 22:02:54 +0000

    Improve index page in anticipation of further updates

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,21 +1,21 @@
 export const languages = {
-    en: "english",
-    ru: "pусский",
+  en: "english",
+  ru: "pусский",
 };
 
 export const defaultLang = "en";
 export const showDefaultLang = false;
 export const ui = {
-    en: {
-        title: "bashout.",
-        contact: "about",
-        home: "home",
-        notFound: "Unfortunately what you're looking for is not here.",
-    },
-    ru: {
-        title: "bashout.",
-        contact: "обо мне",
-        home: "домашняя страница",
-        notFound: "К сожалению, страница не найдена",
-    },
+  en: {
+    title: "bashout.",
+    me: "about",
+    home: "home",
+    notFound: "Unfortunately what you're looking for is not here.",
+  },
+  ru: {
+    title: "bashout.",
+    me: "обо мне",
+    home: "домашняя страница",
+    notFound: "К сожалению, страница не найдена",
+  },
 } as const;
